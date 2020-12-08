3b502057c85401cc5b7076cf5bb490c1bce4338c
Shane Connors <connors.shane@hotmail.com>
2020-12-07 23:12:14 -0500

    style landing page

--- web/src/components/containers/Landing.jsx
+++ web/src/components/containers/Landing.jsx
@@ -1,10 +1,10 @@
-import React from "react"
-
+import React from "react";
 
 export default function Landing() {
-
-
-    return (
-        <div className="bg-coolGray-700"><p>Welcome to Disrupt!</p><p>Create or join a server to start chattting.</p></div>
-    )
-} 
+  return (
+    <div className="bg-coolGray-700 text-teal-50 flex flex-col items-center justify-center">
+      <p className="text-8xl">Welcome to Disrupt!</p>
+      <p className="text-4xl">Create or join a server to start chattting.</p>
+    </div>
+  );
+}
